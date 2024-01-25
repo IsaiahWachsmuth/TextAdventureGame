ae04b2d0821e3e81f942642a1b459cd6327d9ef3
IsaiahWachsmuth <isaiahwachsmuth@gmail.com>
2024-01-25 11:48:50 +0500

    Adding the ability to switch between teacher and student (rough draft)

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -3,44 +3,81 @@ import React, { useState } from 'react';
 const Sidebar = () => {
     const [isLogin, setIsLogin] = useState(true);
 
+    const [isTeacher, setIsTeacher] = useState(true);
+
     const toggleForm = () => {
         setIsLogin(!isLogin);
     };
 
+    const setUserType = () => {
+        setIsTeacher(!isTeacher);
+    };
+
     return (
         <div className='d-flex flex-column main-sidebar'>
-
-            <div className='login-wrapper'>
-                <div id='login-toggle'>
-                    <button onClick={toggleForm}>{isLogin ? "Register" : "Login"}</button>
-                </div>
-                <form>
-                    <div>
-                        <label htmlFor="email">Email: </label>
-                        <input type="email" id="email" name="email" required />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password: </label>
-                        <input type="password" id="password" name="password" required />
-                    </div>
-                    {isLogin ? (
-                        <div className='login-register-submit'>
-                            <button type="submit">Login</button>
-                        </div>
-                    ) : (
-                        <>
-                            <div>
-                                <label htmlFor="confirmPassword">Confirm Password: </label>
-                                <input type="password" id="confirmPassword" name="confirmPassword" required />
-                            </div>
-                            <div className='login-register-submit'>
-                                <button type="submit">Register</button>
-                            </div>
-                        </>
-                    )}
-                </form>
+            <div id='teacher-toggle'>
+                        <button onClick={setUserType}>{isTeacher ? "Go to Student Login" : "Go to Teacher Login"}</button>
             </div>
-        </div>
+
+            {isTeacher ? (
+                <div className='login-wrapper'>
+                    <div className = 'teacher-title'> Teacher Login/Registration </div>
+                    <div id='login-toggle'>
+                            <button onClick={toggleForm}>{isLogin ? "Register" : "Login"}</button>
+                    </div>
+                        
+                    <form>
+                        <div>
+                            <label htmlFor="email">Email: </label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                            
+                        <div>
+                            <label htmlFor="password">Password: </label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                            
+                        {isLogin ? (
+                            <div className='login-register-submit'>
+                                <button type="submit">Login as Teacher</button>
+                            </div>
+                            
+                            ) : (
+                                <>
+                                    <div>
+                                        <label htmlFor="confirmPassword">Confirm Password: </label>
+                                        <input type="password" id="confirmPassword" name="confirmPassword" required />
+                                    </div>
+                                    <div className='login-register-submit'>
+                                        <button type="submit">Register as Teacher</button>
+                                    </div>
+                                </>
+                            )}
+                        </form>
+                        </div>
+                ) : (
+                    <div className = 'studentLogin'> 
+                        <div> Student Login </div>
+                        <form>
+                        <div>
+                            <label htmlFor="name">Name: </label>
+                            <input type="name" id="name" name="name" required />
+                        </div>
+                            
+                        <div>
+                            <label htmlFor="code">Class Code: </label>
+                            <input type="code" id="code" name="code" required />
+                        </div>
+
+                        <div className='student-login'>
+                                            <button type="submit">Login as Student</button>
+                        </div>
+                        </form>
+                    </div>
+                )}
+                
+                </div>
+    
     );
 };
 
