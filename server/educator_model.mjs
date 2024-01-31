f669a6683dc27cd7bb2973b21a3fd65b8f62162e
calsbrook <CameronAlsbrook@gmail.com>
2024-01-30 22:12:26 -0800

    added bcrypt and used it in the educator model

--- server/educator_model.mjs
+++ server/educator_model.mjs
@@ -1,13 +1,26 @@
 // EducatorModel.mjs
 import mongoose from 'mongoose';
-
+const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 
 const educatorSchema = new Schema({
   _id: { type: Schema.Types.ObjectId, required: true, auto: true },
-  name: { type: String, required: true },
+  name: { type: String, min: 1, max: 100},
+  email: { type: String, required: true },
   students: [{ type: Schema.Types.ObjectId, ref: 'Student' }],
   adventures: [{ type: Schema.Types.ObjectId, ref: 'Adventure' }]
+});
+educatorSchema.pre('save',function(next){
+  if(!this.isModified('password')) {
+    return next();
+  }
+  bcrypt.hash(this.password, 10, (err, passwordHash) => {
+    if(err) {
+      return next(err);
+    }
+    this.password = passwordHash;
+    next();
+  });
 });
 
 export const Educator = mongoose.model('Educator', educatorSchema);
