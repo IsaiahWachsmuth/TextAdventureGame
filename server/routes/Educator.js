1a21106ce3547900386864db3bfd11e4fd6e8748
calsbrook <CameronAlsbrook@gmail.com>
2024-02-06 21:50:18 -0800

    got rid of routes files not being used

--- server/routes/Educator.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const educatorRouter = express.Router();
-const passport = require('passport');
-const passportConfig = require('../passport.mjs');
-const Educator = require('../educator_model.mjs');
-const JWT = require('jsonwebtoken');
-
-educatorRouter.post();
