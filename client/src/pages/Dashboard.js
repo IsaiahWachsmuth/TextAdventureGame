1aa30903eae3ad11a89b3c8a92923abc382c7ed9
Adam Ollar <97258986+Ollaradam@users.noreply.github.com>
2024-02-26 06:25:59 -0800

    Games have details
    Dashboard now works with dashboardgrid and gamedetails component to display and navigate to details

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import TopNavDashboard from '../components/TopNavDashboard';
 import DashboardGrid from '../components/DashboardGrid';
 import AddGame from '../components/AddGame';
+import GameDetails from '../components/GameDetails'; 
 
 const Dashboard = () => {
-    const [games, setGames] = useState([]); // Initialize games state
-    const [currentView, setCurrentView] = useState('list'); // 'list' or 'addGame'
+    const [games, setGames] = useState([]);
+    const [currentView, setCurrentView] = useState('list'); // Add a new state for 'gameDetails'
+    const [selectedGame, setSelectedGame] = useState(null); // State to hold the selected game's details
 
     useEffect(() => {
         if (currentView === 'list') {
@@ -27,13 +29,22 @@ const Dashboard = () => {
         setCurrentView('addGame');
     };
 
+    const handleGameSelect = (game) => {
+        setSelectedGame(game);
+        setCurrentView('gameDetails'); // Change to the game details view
+    };
+
     return (
         <div className="d-flex dashboard" id="temp-ed-dash">
             <TopNavDashboard />
-            {currentView === 'list' ? (
-                <DashboardGrid games={games} onAddGame={handleAddGameClick} />
-            ) : (
+            {currentView === 'list' && (
+                <DashboardGrid games={games} onAddGame={handleAddGameClick} onGameSelect={handleGameSelect} />
+            )}
+            {currentView === 'addGame' && (
                 <AddGame onBack={() => setCurrentView('list')} />
+            )}
+            {currentView === 'gameDetails' && selectedGame && (
+                <GameDetails game={selectedGame} onBack={() => setCurrentView('list')} />
             )}
         </div>
     );
