64667bd95abc7cf28339f6ddf78459ba346955e6
IsaiahWachsmuth <isaiahwachsmuth@gmail.com>
2024-03-06 12:28:32 +0500

    Adding PlayGame Page

---
+++ client/src/pages/PlayGame.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const PlayGamePage = () => {
+
+    return (
+        <div>
+            <h2>Play Game</h2>
+        </div>
+    );
+}
+export default PlayGamePage;
+    
