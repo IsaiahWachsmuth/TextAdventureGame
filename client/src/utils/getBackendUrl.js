dba675d58c00fc6ceb7020f58dad487c5b457f7d
calsbrook <CameronAlsbrook@gmail.com>
2024-05-23 22:10:42 -0700

    probably final push for hosting

--- client/src/utils/getBackendUrl.js
+++ client/src/utils/getBackendUrl.js
@@ -1,19 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-
 const getBackendUrl = () => {
-  const filePath = path.resolve(__dirname, '../../.production_variables.json');
-  try {
-    if (fs.existsSync(filePath)) {
-      const fileContent = fs.readFileSync(filePath, 'utf-8');
-      const config = JSON.parse(fileContent);
-      return config.backend_url || 'http://localhost:3001';
-    } else {
-      return 'http://localhost:3001';
-    }
-  } catch (err) {
-    console.error('Error reading .production_variables.json:', err);
-    return 'http://localhost:3001';
+  if (process.env.NODE_ENV === 'production') {
+    return "https://textadventuregameforeducation.online:3001"
+  } else {
+    return "http://localhost:3001"
   }
 };
 
--- server/middleware/getFrontendUrl.mjs
+++ server/middleware/getFrontendUrl.mjs
@@ -1,21 +1,5 @@
-// middleware/getFrontendUrl.js
-import fs from 'fs'
-import path from 'path'
+const getBackendUrl = () => {
+  return process.env.FRONTEND_URL || "http://localhost:3000";
+};
 
-const getFrontendUrl = () => {
-    const filePath = path.resolve(__dirname, '../../.production_variables.json');
-    try {
-      if (fs.existsSync(filePath)) {
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-        const config = JSON.parse(fileContent);
-        return config.frontend_url || 'http://localhost:3000';
-      } else {
-        return 'http://localhost:3000';
-      }
-    } catch (err) {
-      console.error('Error reading .production_variables.json:', err);
-      return 'http://localhost:3000';
-    }
-  };
-
-export default getFrontendUrl;
+export default getBackendUrl;
